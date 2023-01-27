bd8db3ae5763b0f2f84bef4109613aa0f7bca5d3
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2023-01-27 22:26:26 +0400

    delete selected items

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,18 @@ const options = [
   { label: "Fifth", value: 5 },
 ];
 function App() {
-  const [value, setValue] = useState<SelectOption | undefined>(options[0]);
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
   return (
     <>
-      <Select options={options} value={value} onChange={(o) => setValue(o)} />
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
     </>
   );
 }
