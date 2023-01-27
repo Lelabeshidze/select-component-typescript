cecf6096aab7ffcc0f2666cee495397ae4abd937
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2023-01-27 21:09:53 +0400

    created environment

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Select } from "./Select";
+
+
+const options =[
+  {label:"First",value:1},
+  {label:"Second",value:2},
+  {label:"Third",value:3},
+  {label:"Fourth",value:4},
+  {label:"Fifth",value:5},
+
+]
+function App() {
+  return (
+    <>
+      <Select options={options}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Select.tsx
@@ -0,0 +1,27 @@
+import styles from "./select.module.css";
+type SelectOption = {
+  label: string;
+  value: any;
+};
+type SelectProps = {
+  options: SelectOption;
+  onChange(value: SelectOption | undefined): void;
+  value?: SelectOption;
+};
+export function Select({ value, onChange, options }: SelectProps) {
+  return (
+    <div tabIndex={0} className={styles.container}>
+      <span className={styles.value}></span>
+      <button className={styles["clear-btn"]}> &times;</button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul className={styles.options}>
+        {options.map((option) => (
+          <li key={option.label} className={styles.option}>
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
